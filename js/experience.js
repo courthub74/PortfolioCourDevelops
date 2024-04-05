eb06cd87432520c1631dbcd5b0e2c5c3a49c4f22
Courdevelops <courdevelops@gmail.com>
2024-04-05 16:13:46 -0400

    experience 1500 hover done

--- js/experience.js
+++ js/experience.js
@@ -15,7 +15,7 @@ let job_1_description = document.getElementById('job_1_description');
 let job_1_details = document.getElementById('exp_details_one');
 
 // click or hover
-job_1_square.addEventListener('hover', function() {
+job_1_square.addEventListener('mouseover', function() {
     // make job description dissapear
     // job_1_description.classList.add('dissapear');
     // make job deets appear
@@ -24,5 +24,68 @@ job_1_square.addEventListener('hover', function() {
     // function job_1_reset() {
 
     // }
+});
+
+// remove when removing mouse
+job_1_square.addEventListener('mouseout', function() { 
+    job_1_details.classList.remove('show');
+});
+
+// query the square
+    // to activate sequence
+let job_2_square = document.getElementById('job_2');
+
+// query the job description
+    // to make dissapear
+let job_2_description = document.getElementById('job_2_description');
+
+// query the job deets
+    // to make appear
+let job_2_details = document.getElementById('exp_details_two');
+
+// click or hover
+job_2_square.addEventListener('mouseover', function() {
+    // make job description dissapear
+    // job_1_description.classList.add('dissapear');
+    // make job deets appear
+    job_2_details.classList.add('show');
+    // function to reset it all
+    // function job_1_reset() {
+
+    // }
+});
+
+// remove when removing mouse
+job_2_square.addEventListener('mouseout', function() { 
+    job_2_details.classList.remove('show');
+});
+
+// query the square
+    // to activate sequence
+    let job_3_square = document.getElementById('job_3');
+
+    // query the job description
+        // to make dissapear
+    let job_3_description = document.getElementById('job_3_description');
+    
+    // query the job deets
+        // to make appear
+    let job_3_details = document.getElementById('exp_details_three');
+
+// click or hover
+job_3_square.addEventListener('mouseover', function() {
+    // make job description dissapear
+    // job_1_description.classList.add('dissapear');
+    // make job deets appear
+    job_3_details.classList.add('show');
+    // function to reset it all
+    // function job_1_reset() {
+
+    // }
+});
+
+// remove when removing mouse
+job_3_square.addEventListener('mouseout', function() { 
+    job_3_details.classList.remove('show');
 });
 
